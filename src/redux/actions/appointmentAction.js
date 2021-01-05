64850c74262157769c5ca5844d596b385b63a897
Naufal Al Fachri <nafa.create@gmail.com>
2021-01-05 13:23:13 +0700

    create action for change status done and update diagnose

--- src/redux/actions/appointmentAction.js
+++ src/redux/actions/appointmentAction.js
@@ -1,18 +1,71 @@
 import API from "../../API/mainServer";
 
-const changeStatusCheckout = (status, appointment_id, accesstoken) => async (
+const changeStatusPaid = (status, appointment_id, accesstoken) => async (
   dispatch
 ) => {
-  const changeStatusCheckout = await API({
-    url: `/appointments/status/${appointment_id}`,
-    method: "PATCH",
-    headers: {
-      accesstoken: accesstoken,
-    },
-    data: {
-      status: status,
-    },
-  });
+  try {
+    const changeStatusPaid = await API({
+      method: "PATCH",
+      url: `/appointments/status/${appointment_id}`,
+      headers: {
+        accesstoken: accesstoken,
+      },
+      data: {
+        status: status,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export { changeStatusCheckout };
+const changeStatusDone = (status, appointment_id, accesstoken) => async (
+  dispatch
+) => {
+  try {
+    const changeStatusDone = await API({
+      // => fungsi ini di panggil di psikaiter dashborard, setiap kali psikiater tekan tombol selesai makan akan menjalankan fungsi ini
+      method: "PATCH",
+      url: `/appointments/status/${appointment_id}`,
+      headers: {
+        accesstoken: accesstoken,
+      },
+      data: {
+        status: status, // => expect status yang dikirim oleh psikiater "done"S
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addDiagnosePatient = (
+  diagnose_name,
+  diagnose_time,
+  accesstoken,
+  appointment_id
+) => async (dispatch) => {
+  try {
+    const addDiagnosePatient = await API({
+      method: "PATCH",
+      url: `/appointments/diagnose/${appointment_id}`,
+      headers: {
+        accesstoken: accesstoken,
+      },
+      body: {
+        diagnose_name: diagnose_name,
+        diagnose_time: diagnose_time,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const appointmentAction = {
+  changeStatusPaid,
+  changeStatusDone,
+  addDiagnosePatient,
+};
+
+export default appointmentAction;
